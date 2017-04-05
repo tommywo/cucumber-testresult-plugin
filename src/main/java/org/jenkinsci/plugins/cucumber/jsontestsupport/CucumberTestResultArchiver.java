9805caa16e50ce45e58ee330e2357c05d8a465b7
Tomasz Wojtuń <wojtut01@stepstone.com>
2017-04-05 10:32:25 +0200

    don’t set Result.FAILURE, when all tests failed.

--- src/main/java/org/jenkinsci/plugins/cucumber/jsontestsupport/CucumberTestResultArchiver.java
+++ src/main/java/org/jenkinsci/plugins/cucumber/jsontestsupport/CucumberTestResultArchiver.java
@@ -182,9 +182,7 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 		if (result.getPassCount() == 0 && result.getFailCount() == 0 && result.getSkipCount() == 0)
 			throw new AbortException("No cucumber scenarios appear to have been run.");
 
-		if (action.getResult().getTotalCount() == action.getResult().getFailCount()){
-			build.setResult(Result.FAILURE);
-		} else if (action.getResult().getFailCount() > 0) {
+		if (action.getResult().getFailCount() > 0) {
 			build.setResult(Result.UNSTABLE);
 		}
 
